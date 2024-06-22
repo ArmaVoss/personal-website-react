b07558cd5be6a23abb4d6bb751ff785c36bd48c6
Arman Vossoughi <armanvossoughi@Armans-MacBook-Pro.local>
2024-06-22 13:26:01 -0700

    changes

--- personal-website/src/components/landing-page.jsx
+++ personal-website/src/components/landing-page.jsx
@@ -3,9 +3,10 @@ function LandingPage() {
       <div className="bg-customBackgroundColor w-full h-screen">
         <div className="flex justify-center h-full">
             <div className = "flex-column text-center mt-80">
-                <h1 className="text-customGreen text-3xl">hi</h1>
-                <h1 className ="text-white text-4xl">I'm Arman</h1>
-                <h1 className ="text-customGrayLight text-2xl">this is my website</h1>
+                <h1 className="text-white text-4xl">Welcome!</h1>
+                <h1 className ="text-customOrange text-4xl">I'm Arman</h1>
+                <h1 className ="text-customGrayLight text-xl">this is my website</h1>
+                <h1 className ="inline-block text-white text-xl bg-transparent px-4 py-1 border-2 border-customOrange mt-40 cursor-pointer">Click Me</h1>
             </div>
         </div> 
       </div>
